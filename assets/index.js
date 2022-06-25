f3f828ea9201ecee8e5bb9c3578ca51a14be9218
Felix Cagatin <cagatingilbert@gmail.com>
2022-06-25 08:38:49 -0700

    Added index.html page to the server

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: '../../Roadtrip' });
+})
+
 app.listen(3000, () => {
     console.log("test");
 })
