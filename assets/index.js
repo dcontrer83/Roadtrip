b11de95d8ce20f408387756c7f4b156b97ebd171
Felix Cagatin <cagatingilbert@gmail.com>
2022-06-25 09:23:46 -0700

    tried adding stylesheet to server

--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
+//Render anything within the assets folder
+app.use(express.static(path.join(__dirname)));
+
+//Display index.html page when user enters the homepage
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: '../../Roadtrip' });
 })
 
+//create server at port 3000
 app.listen(3000, () => {
     console.log("test");
 })
